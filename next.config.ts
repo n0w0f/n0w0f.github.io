9531a1df9904012f98b02471e05fe35568db3e0a
n0w0f <pvt.nawaf@gmail.com>
2025-04-03 10:47:57 +0530

    chore: supoport other patterns for images

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,14 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "lamalab.org",
+      },
+      {
+        protocol: "https",
+        hostname: "github.com",
+      },
     ],
   },
 };
