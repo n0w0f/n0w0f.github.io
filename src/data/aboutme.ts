6a9f046334c1c2347c5fe9ed07888006f8a043f1
n0w0f <pvt.nawaf@gmail.com>
2025-04-03 10:48:16 +0530

    chore: add bio

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -17,23 +17,27 @@ export interface AboutMe {
   institutionUrl?: string;
 }
 
+
 export const aboutMe: AboutMe = {
-  name: "Jane R. Smith",
-  title: "Ph.D. Candidate",
-  institution: "Stanford University",
+  name: "Nawaf Alampara",
+  title: "Doctoral Researcher",
+  institution: "Friedrich-Schiller-Universität Jena",
   // Note that links work in the description
   description:
-    "I'm a final-year <a href='https://www.stanford.edu'>PhD candidate</a> working at the intersection of causal inference and machine learning. My research focuses on developing robust, interpretable systems that can reason about cause and effect in complex environments.",
-  email: "______@stanford.edu",
+    `I am second-year PhD student, working with <a href='https://kjablonka.com/'>Dr. Kevin Maik Jablonka</a>.
+     I'm building machine learning systems to speed up scientific research, and I love projects that involve both research and building the tooling that enables and accelerates that research.
+     Lately, I’ve been analyzing general-purpose AI models/systems to understand their limitations in scientific applications—where they fail—and interpreting them to uncover why they fail.
+     My goal is to use these insights to design AI systems that aren’t just impressive on benchmarks but truly impactful for advancing science and research.`,
+  email: "pvt.nawaf@gmail.com",
   imageUrl:
-    "https://images.unsplash.com/photo-1581481615985-ba4775734a9b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  googleScholarUrl: "https://scholar.google.com/citations?user=bWtMl_MAAAAJ",
-  githubUsername: "janesmith",
-  linkedinUsername: "janesmith",
-  twitterUsername: "janesmith",
-  blogUrl: "https://",
+    "https://lamalab.org/images/team/nawaf.png",
+  googleScholarUrl: "https://scholar.google.com/citations?user=R7K3xFIAAAAJ&hl=en",
+  githubUsername: "n0w0f",
+  linkedinUsername: "n0w0f",
+  twitterUsername: "Iam_Nawaf_",
+  // blogUrl: "https://",
   cvUrl: "https://",
-  institutionUrl: "https://www.stanford.edu",
+  institutionUrl: "https://www.uni-jena.de/en",
   // altName: "",
   // secretDescription: "I like dogs.",
 };
