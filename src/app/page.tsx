ad9f95f4ce12f1315d85d84c7b9e88af72e6a608
n0w0f <pvt.nawaf@gmail.com>
2025-04-03 10:48:34 +0530

    chore: inc font size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,14 +31,19 @@ export default function Home() {
           <div className="col-span-12 md:col-span-7 md:col-start-6 space-y-24">
             {/* About section is typically first */}
             {aboutMe.description && (
+              // <section>
+              //   <p
+              //     className="font-serif text-sm leading-relaxed text-zinc-700 [&_a]:underline [&_a]:text-zinc-900 [&_a:hover]:text-zinc-600"
+              //     dangerouslySetInnerHTML={{ __html: aboutMe.description }}
+              //   />
+              // </section>
               <section>
-                <p
-                  className="font-serif text-sm leading-relaxed text-zinc-700 [&_a]:underline [&_a]:text-zinc-900 [&_a:hover]:text-zinc-600"
-                  dangerouslySetInnerHTML={{ __html: aboutMe.description }}
-                />
+              <p
+                className="font-serif text-base leading-relaxed text-zinc-700 [&_a]:underline [&_a]:text-zinc-900 [&_a:hover]:text-zinc-600"
+                dangerouslySetInnerHTML={{ __html: aboutMe.description }}
+              />
               </section>
             )}
-
             {/* Map through sectionOrder to render sections in correct order */}
             {sectionOrder.map((sectionName) => {
               // Most of this is redundant... but in case it needs to be unique.
