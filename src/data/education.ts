a27d2580745ec1b0f6337775769c6f69f3b035e8
n0w0f <pvt.nawaf@gmail.com>
2025-04-03 10:48:52 +0530

    chore: education

--- src/data/education.ts
+++ src/data/education.ts
@@ -8,19 +8,22 @@ export interface Education {
 }
 
 export const educationData: Education[] = [
-  // If you don't want to show education, just make the array empty.
   {
-    year: "2021—Present",
-    institution: "Stanford University",
-    degree: "Ph.D. in Computer Science",
-    advisor: "Prof. Sarah Johnson",
+    year: "2023—2026",
+    institution: "Friedrich-Schiller-Universität Jena, Germany",
+    degree: "PhD Machine Learning for Science",
+    advisor: "Dr. Kevin Maik Jablonka",
   },
   {
-    year: "2017—2021",
-    institution: "Massachusetts Institute of Technology",
-    degree: "B.S. in Computer Science and Mathematics",
-    thesis: "Algorithmic Approaches to Causal Discovery",
-    // Optional links to thesis
-    // thesisUrl: "https://dspace.mit.edu/handle/1721.1/149111"
+    year: "2018—2020",
+    institution: "Indian Institute of Technology Bombay, India",
+    degree: "MSc Energy Science",
+    advisor: "Prof. K R Balasubramaniam",
+    thesis: "Defects and Dopants in Cu₂O - DFT study",
+  },
+  {
+    year: "2015—2018",
+    institution: "Birla Institute of Technology Mesra, India",
+    degree: "BSc Physics",
   },
 ];
