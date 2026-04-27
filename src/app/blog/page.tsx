f13da6387ba66766c8f3ef66fa71579535455d7d
n0w0f <pvt.nawaf@gmail.com>
2026-04-27 15:07:39 -0400

    feat(blog): add blog index page grouped by year

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,18 +1,6 @@
 import { blogPosts } from "@/data/blog";
+import { BlogIndex } from "@/components/blog/blog-index";
 
 export default function BlogIndexPage() {
-  return (
-    <div className="min-h-screen bg-[#FFFCF8]">
-      <div className="max-w-screen-lg mx-auto px-8 py-24">
-        <h1 className="font-serif text-4xl text-zinc-900">Writing</h1>
-        <ul className="mt-8 space-y-4">
-          {blogPosts.map((p) => (
-            <li key={p.slug} className="font-serif text-zinc-700">
-              {p.title}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
+  return <BlogIndex posts={blogPosts} />;
 }
